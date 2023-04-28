9c41e5f912c2aa7bdda8d39bb249cb7dce3af916
henry <773542531@qq.com>
2023-04-28 16:18:01 +0800

    add slide

--- hw/hw1/harris.py
+++ hw/hw1/harris.py
@@ -13,7 +13,7 @@ sobely = cv2.convertScaleAbs(sobely)
 
 # 3. get I_xI_y
 # there are two ways to get I_xI_y
-sobelxy =  cv2.addWeighted(sobelx,0.5,sobely,0.5,0)
+sobelxy = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
 # sobelxy = cv2.Sobel(o, cv2.CV_64F, 1, 1)
 # sobelxy = cv2.convertScaleAbs(sobelxy)
 
@@ -25,28 +25,30 @@ sobelxy =  cv2.addWeighted(sobelx,0.5,sobely,0.5,0)
 
 # 5. calculate the eigenvalue and eigenvector of M
 # we use np.linalg.eig(A) to get eigenvalue and eigenvector
+
+# M has to change, M = np.zeros([o.shape[0]-windowSize+1, o.shape[1]-windowSize+1, 2, 2])
 M = np.zeros([o.shape[0], o.shape[1], 2, 2])
 valueM = np.zeros([o.shape[0], o.shape[1], 2, 2])
 R = np.zeros(o.shape)
 k = 0.04
 for i in range(o.shape[0]):
     for j in range(o.shape[1]):
-        xx = sobelx[i][j]**2
-        yy = sobely[i][j]**2
+        xx = sobelx[i][j] ** 2
+        yy = sobely[i][j] ** 2
         xy = sobelxy[i][j]
 
         M[i][j] = np.array([[xx, xy], [xy, yy]])
         valueM[i][j], _ = np.linalg.eig(np.array([[xx, xy], [xy, yy]]))
 
-# 6. calculate R by R = l1*l2 - k(l1+l2)^2
+        # 6. calculate R by R = l1*l2 - k(l1+l2)^2
         l1 = valueM[i][j][0][0]
         l2 = valueM[i][j][0][1]
-        R[i][j] = l1*l2 - k*(l1+l2)**2
+        R[i][j] = l1 * l2 - k * (l1 + l2) ** 2
 
 # 7. use threshold to judge
 for i in range(o.shape[0]):
     for j in range(o.shape[1]):
-        if(R[i][j] > R.max()*0.01):
+        if R[i][j] > R.max() * 0.01:
             img[i][j] = (0, 0, 255)
 
 # 8. show img
